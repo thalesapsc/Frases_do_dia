b81a8101c7c73c7b7c90f81e2ff80a00503d8778
Thales Aparecido Silva Carneiro <thalesapscarneiro@gmail.com>
2022-04-27 23:38:10 -0300

    Aplicativo frases do dia

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const MyHomePage(title: 'Sua frase'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _numeroAleatorio = 0;
+  List _frases = [
+    '',
+    'Tenha Brio',
+    'Não desista',
+    'Faça mesmo sem vontade',
+    'Tenha força de vontade'
+  ];
+  void _incrementCounter() {
+    setState(() {
+      _numeroAleatorio = new Random().nextInt(5);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Frase do dia:',
+            ),
+            Text(
+              _frases[_numeroAleatorio],
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
